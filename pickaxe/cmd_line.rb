0fb60f3262c7cae2ab6be44023c579dc1d3f1064
sambit <34sambit@gmail.com>
2011-10-28 09:56:18 -0700

    removed all files in order to reorganize

--- pickaxe/cmd_line.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/local/bin/ruby -w
-
-puts "You gave #{ARGV.size} arguments"
-p ARGV
-
--- pickaxe/intro-control-structures.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/ruby -w
-
-def SquaresAndStiffs
-  @@square=2
-  square = square*square 
-  puts #{square}
-  while square < 1000
-end
